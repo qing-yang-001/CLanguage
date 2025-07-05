61278ed2dd09024ded3414994561e8d9dc8783be
BTX\baitongxin <2861473220qq.com>
2025-07-05 21:41:40 +0800

    添加注释

--- 001-HelloWorld/HelloWorld.c
+++ 001-HelloWorld/HelloWorld.c
@@ -1,5 +1,14 @@
+/*
+ * @Author: BTX\baitongxin 2861473220qq.com
+ * @Date: 2025-07-05 20:22:48
+ * @LastEditors: BTX\baitongxin 2861473220qq.com
+ * @LastEditTime: 2025-07-05 21:33:14
+ * @FilePath: \CLanguage\001-HelloWorld\HelloWorld.c
+ * @Description: 第一次hello world代码
+ */
+
 #include<stdio.h>
 int main(){
-    printf("HellowWorld!\n");
+    printf("HellowWorld!\nByebye,World!\n");
     return 0;
 }
