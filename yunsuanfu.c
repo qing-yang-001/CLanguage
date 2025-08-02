c5e1fb0235eef328a6057b12fb7dda83f57cda7a
BTX\baitongxin <2861473220qq.com>
2025-08-02 16:34:38 +0800

    feat: 添加a++和++a的区别示例代码
    在代码中新增了一个名为`yunsuanfu.c`的文件，该文件包含一个简单的C语言程序，用于演示`a++`和`++a`的区别。程序定义了两个整数变量`a`和`c`，首先通过`a++`的方式进行运算，然后通过`++a`的方式进行运算，并分别打印出运算前后`a`和`c`的值，以便用户理解这两种自增操作符的行为差异。

---
+++ yunsuanfu.c
@@ -0,0 +1,22 @@
+/*
+ * @Author: BTX\baitongxin 2861473220qq.com
+ * @Date: 2025-08-02 16:23:00
+ * @LastEditors: BTX\baitongxin 2861473220qq.com
+ * @LastEditTime: 2025-08-02 16:34:13
+ * @FilePath: \CLanguage\yunsuanfu.c
+ * @Description: a++和++a的区别
+ */
+#include <stdio.h>
+
+int main()
+{
+  int a, c;
+  a = 10;
+  c = a++;
+  printf("先赋值后运算\n");
+  printf("a=%d\nc=%d\n", a, c);
+  a=10;
+  c = ++a;
+  printf("先运算后赋值\n");
+  printf("a=%d\nc=%d\n", a, c);
+}
