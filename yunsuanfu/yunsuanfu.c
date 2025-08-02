181969c9350220374e467ba2b52e3fc52e26d45e
BTX\baitongxin <2861473220qq.com>
2025-08-02 16:51:48 +0800

    展示了C语言中自增运算符a++和++a的区别

--- yunsuanfu/yunsuanfu.c
+++ yunsuanfu/yunsuanfu.c
@@ -2,9 +2,9 @@
  * @Author: BTX\baitongxin 2861473220qq.com
  * @Date: 2025-08-02 16:23:00
  * @LastEditors: BTX\baitongxin 2861473220qq.com
- * @LastEditTime: 2025-08-02 16:34:13
- * @FilePath: \CLanguage\yunsuanfu.c
- * @Description: a++和++a的区别
+ * @LastEditTime: 2025-08-02 16:50:52
+ * @FilePath: \CLanguage\yunsuanfu\yunsuanfu.c
+ * @Description: a++和++a的区别.
  */
 #include <stdio.h>
 
@@ -15,7 +15,7 @@ int main()
   c = a++;
   printf("先赋值后运算\n");
   printf("a=%d\nc=%d\n", a, c);
-  a=10;
+  a = 10;
   c = ++a;
   printf("先运算后赋值\n");
   printf("a=%d\nc=%d\n", a, c);
